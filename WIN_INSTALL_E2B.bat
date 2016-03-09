30d4b97d4934150cee456e1cf6b1abc9367e4809
kristyj <kristypjames@gmail.com>
2016-03-09 10:30:09 +0100

    Add bat files for windows execution of gradle commands

---
+++ WIN_INSTALL_E2B.bat
@@ -0,0 +1,6 @@
+echo off
+:: This is a batch file to run the Ematoblender package install with gradle.
+:: It is simply an alternative to running gradlew setup.
+
+echo Running the Windows buildscript for Ematoblender
+gradlew setup
---
+++ WIN_RUN_E2B.bat
@@ -0,0 +1,6 @@
+echo off
+:: This is a batch file to run the core Ematoblender elements 
+:: It is simply an alternative to running gradlew run.
+
+echo Running the Windows run script for Ematoblender
+gradlew run
