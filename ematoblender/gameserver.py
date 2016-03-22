a15a5813973bd5d1ec1088480b781335d659aa38
Kristy James <kristyj@coli.uni-saarland.de>
2016-03-22 22:46:25 +0100

    Add a CL passing capabilities, and second method to run directly, not through GUI

--- ematoblender/gameserver.py
+++ ematoblender/gameserver.py
@@ -1,6 +1,12 @@
 __author__ = 'Kristy'
 
-#import scripts.ema_io.ema_gameserver.gameserver as gs
+import sys
 
-from scripts.ema_io.ema_gameserver import gameserver_gui as gg
-gg.main() # command line args are pre-defined as those in properties file
+#import scripts.ema_io.ema_gameserver.gameserver as gs
+if True:
+	from scripts.ema_io.ema_gameserver import gameserver_gui as gg
+	gg.main(sys.argv[1:]) # command line args are pre-defined as those in properties file
+
+else:
+	from scripts.ema_io.ema_gameserver import gameserver as gs
+	gs.main(sys.argv[1:])
