eb11a67f739a75a4c80277a094854359b34b0ff7
kristyj <kristypjames@gmail.com>
2016-03-29 17:37:35 +0200

    Make batch files work in THIS directory

--- WIN_RUN_E2B.bat
+++ WIN_RUN_E2B.bat
@@ -1,6 +1,7 @@
 echo off
 :: This is a batch file to run the core Ematoblender elements 
 :: It runs the GUIs for the static server and the gameserver
+cd %~dp0%
 echo Running the Windows run script for Ematoblender Static
 pause
 :: Run static data
