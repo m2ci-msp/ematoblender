d54a9a715317c6dd58d347c1a2df4c22c2896b7e
Alexander Hewer <hewer@coli.uni-saarland.de>
2016-03-30 15:18:25 +0200

    reduced verbosity on the command line

--- ematoblender/gameserver.py
+++ ematoblender/gameserver.py
@@ -1,11 +1,12 @@
 __author__ = 'Kristy'
 
-import sys
+import sys, os
 
 #import scripts.ema_io.ema_gameserver.gameserver as gs
 if True:
-	from scripts.ema_io.ema_gameserver import gameserver_gui as gg
-	gg.main(sys.argv[1:]) # command line args are pre-defined as those in properties file
+        from scripts.ema_io.ema_gameserver import gameserver_gui as gg
+        sys.stdout = open(os.devnull, "w")
+        gg.main(sys.argv[1:]) # command line args are pre-defined as those in properties file
 
 else:
 	from scripts.ema_io.ema_gameserver import gameserver as gs
