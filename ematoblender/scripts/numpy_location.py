f7c8175a634091098e93954a65b01c27f8ecc443
Alexander Hewer <hewer@coli.uni-saarland.de>
2016-04-07 10:49:12 +0200

    fixed issue if only python 2 is present

--- ematoblender/scripts/numpy_location.py
+++ ematoblender/scripts/numpy_location.py
@@ -1,2 +1,4 @@
+# ensure that the script can be executed by python 2
+from __future__ import print_function
 import imp
 print(imp.find_module('numpy')[1], end='')
--- ematoblender/scripts/tkinter_version.py
+++ ematoblender/scripts/tkinter_version.py
@@ -1,3 +1,5 @@
+# ensure that the script can be executed by python 2
+from __future__ import print_function
 try:
     import tkinter
     print('SUCCESS', end="")
