36e77e22d4d3cbe322586256c937362b49d8cc08
Alexander Hewer <hewer@coli.uni-saarland.de>
2016-09-29 14:00:19 -0700

    updated python scripts for multilinear model

--- ematoblender/scripts/multilinearmodel/ModelBuilder.py
+++ ematoblender/scripts/multilinearmodel/ModelBuilder.py
@@ -1,3 +1,28 @@
+'''
+   This file is part of the multilinear-model-tools.
+   These tools are meant to derive a multilinear tongue model or
+   PCA palate model from mesh data and work with it.
+
+   Some code of the multilinear-model-tools is based on
+   Timo Bolkart's work on statistical analysis of human face shapes,
+   cf. https://sites.google.com/site/bolkartt/
+
+   Copyright (C) 2016 Alexander Hewer
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
 __author__ = "Alexander Hewer"
 __email__ = "hewer@coli.uni-saarland.de"
 
@@ -18,8 +43,11 @@ class ModelBuilder:
             rawModel = json.load(inputFile)
 
         self.modelData.dimVertex = rawModel['Dimensions']['VertexMode']
-        self.modelData.dimSpeaker = rawModel['Dimensions']['SpeakerMode']
-        self.modelData.dimPhoneme = rawModel['Dimensions']['PhonemeMode']
+        self.modelData.dimSpeaker = rawModel['Dimensions']['TruncatedSpeakerMode']
+        self.modelData.dimPhoneme = rawModel['Dimensions']['TruncatedPhonemeMode']
+
+        self.modelData.dimSpeakerOriginal = rawModel['Dimensions']['OriginalSpeakerMode']
+        self.modelData.dimPhonemeOriginal = rawModel['Dimensions']['OriginalPhonemeMode']
 
         self.modelData.coreTensor = numpy.array(rawModel['CoreTensor'])
         self.modelData.shapeSpaceOrigin = numpy.array(rawModel['ShapeSpace']['Origin'])
